7091fee7a13599d85094265271a77ce925c7dd9d
kriomanser65 <106473729+Kriomanser65@users.noreply.github.com>
2024-03-10 23:09:42 +0200

    first commit

---
+++ javascript.js
@@ -0,0 +1,8 @@
+function openModal()
+{
+    document.getElementById('mModal').style.display = 'block';
+}
+function closeModal()
+{
+    document.getElementById('mModal').style.display = 'none';
+}
